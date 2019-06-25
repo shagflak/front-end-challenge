1bc6a60f89c4242a3e10e78a6e24ddaa477bdb18
Juan Carlos Figueroa Rubiano <juanc.figueroa@stkprojects.com>
2019-06-24 19:55:34 -0500

    Added vuelidate to vue isntance also added to the instance the Vmodal with the configuration to support dinamically created modals and added the directive numericOnly for the company spend field planning to refactor the directive to be moved to his own folder.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import 'Styles/main.scss';
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import VModal from 'vue-js-modal'
 import AppComponent from 'Components/AppComponent.vue';
+
+Vue.use(Vuelidate);
+Vue.use(VModal, { dynamic: true, injectModalsContainer: true });
+
+Vue.directive('numericOnly', {
+  bind(el) {
+    el.addEventListener('keyup', () => {
+      let regex = /^[0-9]*$/
+      if (!regex.test(el.value)) {
+        el.value = el.value.slice(0, -1)
+      }
+    })
+  }
+});
+
 
 new Vue({
   render: createElement => createElement(AppComponent),
