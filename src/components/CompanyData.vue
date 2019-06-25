fdaa2af33ef3ac2eb92deb7a34c5a9c2a70a03b1
Juan Carlos Figueroa Rubiano <juanc.figueroa@stkprojects.com>
2019-06-24 19:58:17 -0500

    Added styling for the errors and added display of the errors in the template and added  the validations object to handle the different vaidations for the models created in the data section

--- src/components/CompanyData.vue
+++ src/components/CompanyData.vue
@@ -16,11 +16,15 @@
                 padding: 10px;
                 display: flex;
                 flex-direction: column;
-                color: #e8c2c2;
                 font-weight: bold;
 
-                label {
+                label.form-field-label {
+                    color: #e8c2c2;
                     margin: 6px 0;
+
+                    &.form-field-error-label {
+                        color: #f57f6c;
+                    }
                 }
 
                 input {
@@ -33,6 +37,12 @@
                     padding: 15px;
                     border-radius: 5px;
                     border: 1px solid #e8c2c2;
+                }
+
+                .form-error {
+                    color: #f57f6c;
+                    font-size: 12px;
+                    margin-top: 7px;
                 }
             }
         }
@@ -50,18 +60,27 @@
         <div id="form-container">
             <ul id="company-data-form" >
                 <li class="company-data-form-field" >
-                    <label for="company-name">COMPANY DATA</label>
+                    <label for="company-name" v-bind:class="{ 'form-field-label': true, 'form-field-error-label': (!$v.companyName.required || !$v.companyName.minLength) }" >
+                        COMPANY DATA
+                    </label>
                     <input type="text" 
                            name="company-name" 
                            id="company-name" 
+                           v-model.trim="companyName"
                            placeholder="e.g. Your Company Name">
+                    <span class="form-error" v-if="!$v.companyName.required" >This field is required</span>
+                    <span class="form-error" v-if="!$v.companyName.minLength" >At least 4 characters-</span>
                 </li>
                 <li class="company-data-form-field" >
-                    <label for="company-spend">COMPANY SPEND</label>
+                    <label for="company-spend" class='form-field-label'>COMPANY SPEND</label>
                     <input type="text" 
                            name="company-spend" 
                            id="company-spend" 
-                           placeholder="e.g. $150,000">
+                           v-model.trim="companySpend"
+                           placeholder="e.g. $150,000"
+                           @blur="formatCurrency()"
+                           @focus="unformatCurrency()"
+                           v-numericOnly>
                 </li>
                 <li class="company-data-form-field" >
                     <label for="company-spend">COMPANY SPEND ABILITY</label>
@@ -77,7 +96,8 @@
                     name="company-name-notes" 
                     id="company-name-notes" 
                     cols="30" 
-                    rows="10"></textarea>
+                    rows="10"
+                    v-on:click="displayTextAreaModal(isDisplayed = true)"></textarea>
                 </li>
                 <li class="company-data-form-field" ></li>
             </ul>
@@ -88,8 +108,46 @@
 
 
 <script>
+import { required, minLength, between, numeric } from 'vuelidate/lib/validators';
+import CompanyDataModal from './CompanyDataModal';
+
 export default {
-    data () {},
+    components: {
+        CompanyDataModal
+    },
+    data () {
+        return {
+            //FORM PROPS
+            companyName: '',
+            companySpend: null,
+            companySpendRaw: null,
+            companySpendFormated: null,
+            // OTHER COMPONENT PROPS
+            isDisplayed: false
+        }
+    },
+    methods: {
+        formatCurrency () {
+            this.companySpendRaw = this.$data.companySpend;
+            let val = (this.$data.companySpendRaw/1).toFixed(2).replace('.', ',')
+            this.$data.companySpendFormated = '$ ' + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+            this.$data.companySpend = this.companySpendFormated;
+        },
+        unformatCurrency () {
+            this.$data.companySpend = this.$data.companySpendRaw;
+        },
+        displayTextAreaModal (isDisplayed) {
+            this.$modal.show(CompanyDataModal, {
+                draggable: true
+            });
+        }
+    },
+    validations: {
+        companyName: {
+            required,
+            minLength: minLength(4),
+        }
+    },
     name: 'company-data'
 }
 </script>
